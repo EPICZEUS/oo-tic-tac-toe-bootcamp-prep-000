1c0bfbef94839880bba565d072d621727ce2a969
Sam B <s77broz@aol.com>
2018-09-07 05:09:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -65,7 +65,7 @@ class TicTacToe
   end
   
   def won?
-    @WIN_COMBINATIONS.detect do |combo|
+    WIN_COMBINATIONS.detect do |combo|
       @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]] && position_taken?(combo[0])
     end
   end
